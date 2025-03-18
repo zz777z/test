4eba30d85f49f524d2b7fa03d0d638bfc5781c90
zz777z <limingliming777@outlook.com>
2025-03-18 14:49:37 +0800

    Add files via upload

---
+++ 05/guagame/gua_scene.js
@@ -0,0 +1,21 @@
+class GuaScene {
+    constructor(game) {
+        this.game = game
+    }
+    static new(game) {
+        var i = new this(game)
+        return i
+    }
+    draw() {
+
+    }
+    update() {
+
+    }
+}
+
+
+
+
+
+
